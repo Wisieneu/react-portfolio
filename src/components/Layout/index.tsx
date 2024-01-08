7cc6c8d6939def5ff22700383aa4982e7664b0b0
wisie <wisnia501@gmail.com>
2024-01-08 01:19:53 +0100

    implement conditional tab icons and titles

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,10 +3,11 @@ import Title from '../Title'
 import Nav from '../Nav'
 import Modal from '../ContactForm'
 
-import { useState } from 'react'
+import { pageType } from '../../shared/types/page.type'
+
+import { useEffect, useState } from 'react'
 
 import './index.scss'
-import { pageType } from '../../shared/types/page.type'
 
 export default function Layout() {
   function isOfTypePageType(keyInput: string): keyInput is pageType {
@@ -26,6 +27,31 @@ export default function Layout() {
 
   const [currentPage, setCurrentPage] = useState<pageType>(getPathName())
   const [isModalOpen, toggleModal] = useState(false)
+
+  // Changes site's tab icon and title based on currentPage state
+  useEffect(() => {
+    const favicon = document.querySelector(
+      "link[rel~='icon']"
+    ) as HTMLLinkElement
+
+    switch (currentPage) {
+      case 'About':
+        document.title = 'About me?'
+        favicon.href = '/icon_goose.png'
+        break
+      case 'Contact':
+        document.title = 'Send me an e-mail right now!!!!!!!!!!!'
+        favicon.href = '/icon_rat.png'
+        break
+      case 'Projects':
+        document.title = 'My protégés'
+        favicon.href = '/icon_cat.png'
+        break
+      default:
+        document.title = "Bartłomiej's Portfolio"
+        favicon.href = '/icon_capybara.gif'
+    }
+  }, [currentPage])
 
   function switchPage(pageName: pageType) {
     setCurrentPage(pageName)
